4c6fcb55ffd38a4ff881330d62df72269356b8f5
邬建平 <11685251+southwind957@user.noreply.gitee.com>
2025-01-19 15:42:42 +0800

    perf: 将UI框架从uview-plus更换为wot-design-uni

--- src/pages/index/homePage.vue
+++ src/pages/index/homePage.vue
@@ -2,7 +2,15 @@
   <view class="flex">
     <view class="color-red">{{ title }}</view>
     <view class="title">{{ title }}</view>
+    <wd-button type="success">主要按钮</wd-button>
   </view>
+  <wd-icon name="add-circle" />
+  <wd-tag custom-class="space">标签</wd-tag>
+  <wd-tag custom-class="space" type="primary">标签</wd-tag>
+  <wd-tag custom-class="space" type="danger">标签</wd-tag>
+  <wd-tag custom-class="space" type="warning">标签</wd-tag>
+  <wd-tag custom-class="space" type="success">标签</wd-tag>
+  <wd-watermark content="uni-vt" :width="130" :height="130"></wd-watermark>
 </template>
 <script setup lang="ts">
 import { ref } from 'vue'
