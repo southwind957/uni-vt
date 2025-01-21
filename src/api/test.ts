9f2d751da2ee0bb31ecc1cbd7240c69b2b67f9a0
张佳佳 <11685251+southwind957@user.noreply.gitee.com>
2025-01-21 20:35:31 +0800

    perf: 初步优化文档

--- src/api/test.ts
+++ src/api/test.ts
@@ -4,6 +4,6 @@ export const getTest = <T>(): Promise<IResponse<IList<T>>> => {
   return Request.get('/imgs')
 }
 
-export const TestMock = <T>(): Promise<IResponse<IList<T>>> => {
+export const TestMock = <T>(): Promise<IResponse<T>> => {
   return Request.get('/mock/test')
 }
