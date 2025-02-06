c000c9786df60d47201cf101d2f434ada18d605d
jianping.wu <jianping.wujianping.wu@yangguanghaina.com>
2025-02-06 17:56:01 +0800

    feat: 新增常用样式与表单规则钩子

--- src/hooks/useValidate.ts
+++ src/hooks/useValidate.ts
@@ -1,1 +1,45 @@
 // 这里是Validate的钩子，提供常用的钩子校验规则
+export function useValidateRule() {
+  // 闭包函数生成单个规则
+  const generate = (options: RuleOptions): ValidateRule[] => {
+    switch (options.checkType) {
+      case 'Defalut':
+        return [
+          {
+            required: true,
+            message: options.message
+          }
+        ]
+      case 'RegExp':
+        return [
+          {
+            required: true,
+            message: options.message,
+            pattern: options.pattern
+          }
+        ]
+      default:
+        return [
+          {
+            required: true,
+            message: options.message,
+            validator: options.validator
+          }
+        ]
+    }
+  }
+
+  // 生成整个规则
+  const Rule = (
+    options: RuleOptions[]
+  ): {
+    [key: string]: ValidateRule[]
+  } => {
+    const rules: { [key: string]: ValidateRule[] } = {}
+    for (const option of options) {
+      rules[option.field] = generate(option)
+    }
+    return rules
+  }
+  return { Rule }
+}
--- types/hooks/index.d.ts
+++ types/hooks/index.d.ts
@@ -2,5 +2,15 @@ interface ValidateRule<T = any> {
   required: boolean
   message: string
   validator?: (value: any, rule: any) => boolean | Promise<T>
-  pattern: RegExp
+  pattern?: RegExp
+}
+
+type checkEnum = 'Defalut' | 'RegExp' | 'Function'
+
+interface RuleOptions {
+  message: string
+  validator?: (value: any) => boolean | Promise<boolean>
+  pattern?: RegExp
+  checkType: checkEnum
+  field: string
 }
